4c43378b02818c76dcdc8e7a2361c785e36813a0
KhenCahyo13 <khencahyo02@gmail.com>
2024-11-20 18:39:15 +0700

    refactor: Change .env file dir

--- public/index.php
+++ public/index.php
@@ -1,9 +1,15 @@
 <?php
     use Dotenv\Dotenv;
+    use Dotenv\Exception\InvalidPathException;
 
     require_once '../vendor/autoload.php';
     require_once '../routes/api.php';
 
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
-    $dotenv->load();
+    try {
+        $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+        $dotenv->load();
+    } catch (InvalidPathException $e) {
+        echo "Error: " . $e->getMessage();
+        exit();
+    }
 ?>
