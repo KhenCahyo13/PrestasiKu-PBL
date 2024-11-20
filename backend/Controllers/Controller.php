badaeb465e759e7387b8f2dafcb83034924a5254
KhenCahyo13 <khencahyo02@gmail.com>
2024-11-20 08:08:56 +0700

    ci: Create base Controller class

---
+++ backend/Controllers/Controller.php
@@ -0,0 +1,18 @@
+<?php
+    abstract class Controller {
+        // For get all data
+        abstract function index();
+
+        // For get data by id
+        abstract function show($id);
+
+        // For create data
+        abstract function store();
+
+        // For update data
+        abstract function update($id);
+
+        // For delete data
+        abstract function destroy($id);
+    }
+?>
