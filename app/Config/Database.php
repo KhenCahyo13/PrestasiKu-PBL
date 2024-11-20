710e8574e17b95a7ea1688caa14eaf5ce3d90487
KhenCahyo13 <khencahyo02@gmail.com>
2024-11-20 09:55:46 +0700

    refactor: Database attribute and method return type

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -5,28 +5,28 @@
     use PDOException;
 
     class Database {
-        private $host = "KHOIRUL"; 
-        private $dbName = "prestasiku_db";
-        private $username = ""; 
-        private $password = "";
-        private $conn;
+        private string $dbHost = "KHOIRUL"; 
+        private string $dbName = "prestasiku_db";
+        private string $dbUsername = ""; 
+        private string $dbPassword = "";
+        private PDO $dbConnection;
 
         public function __construct() {
             try {
-                $dsn = "sqlsrv:server=$this->host;Database=$this->dbName";
-                $this->conn = new PDO($dsn, $this->username, $this->password);
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $dsn = "sqlsrv:server=$this->dbHost;Database=$this->dbName";
+                $this->dbConnection = new PDO($dsn, $this->dbUsername, $this->dbPassword);
+                $this->dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die("Connection failed: " . $e->getMessage());
             }
         }
 
-        public function getConnection() {
-            return $this->conn;
+        public function getConnection(): PDO {
+            return $this->dbConnection;
         }
 
-        public function close() {
-            $this->conn = null;
+        public function close(): void {
+            $this->dbConnection = null;
         }
     }
 ?>
