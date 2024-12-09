7bc0909d6c31de8c4f500730ff3e106ca978dbbb
KhenCahyo13 <khencahyo02@gmail.com>
2024-12-09 21:12:17 +0700

    styling(page): Achievement - Add New

--- resources/views/pages/achievement/add-new.php
+++ resources/views/pages/achievement/add-new.php
@@ -19,13 +19,138 @@ renderComponent('breadcrumb', [
 <!-- Card Details Informations -->
 <section class="mt-1 bg-white rounded shadow-sm">
     <div class="px-4 py-3 border-bottom border-secondary">
-        <h2 class="my-0 heading-6">Account Details</h2>
+        <h2 class="my-0 text-base font-semibold">Achievement Informations</h2>
     </div>
-    <div class="px-4 py-3 gy-3 row" id="accountDetailsContainer"></div>
+    <div class="row gx-3 gy-2 px-4 py-3">
+        <div class="col-12">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementTitle" class="text-sm">Title <span class="text-danger">*</span></label>
+                <input type="text" class="form-control form-control-sm" placeholder="Enter your achievement title" id="achievementTitle">
+                <span class="text-xs text-danger" id="achievementTitleError"></span>
+            </div>
+        </div>
+        <div class="col-12">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementDescription" class="text-sm">Description <span class="text-danger">*</span></label>
+                <textarea id="achievementDescription" class="form-control form-control-sm" placeholder="Enter your achievement description" rows="6"></textarea>
+                <span class="text-xs text-danger" id="achievementDescriptionError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementEventLocation" class="text-sm">Event Location <span class="text-danger">*</span></label>
+                <input type="text" class="form-control form-control-sm" placeholder="Eg. State Polytechnic of Malang" id="achievementEventLocation">
+                <span class="text-xs text-danger" id="achievementEventLocationError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementEventCity" class="text-sm">Event City <span class="text-danger">*</span></label>
+                <input type="text" class="form-control form-control-sm" placeholder="Eg. Malang" id="achievementEventCity">
+                <span class="text-xs text-danger" id="achievementEventCityError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementType" class="text-sm">Type <span class="text-danger">*</span></label>
+                <select id="achievementType" class="form-control form-control-sm">
+                    <option value="">- Select achievement type</option>
+                    <option value="Kompetisi">Kompetisi</option>
+                </select>
+                <span class="text-xs text-danger" id="achievementTypeError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementScope" class="text-sm">Scope <span class="text-danger">*</span></label>
+                <select id="achievementScope" class="form-control form-control-sm">
+                    <option value="">- Select achievement scope</option>
+                    <option value="International">International</option>
+                    <option value="National">National</option>
+                    <option value="Regional">Regional</option>
+                </select>
+                <span class="text-xs text-danger" id="achievementScopeError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+            <div class="d-flex flex-column gap-2">
+                <label for="categoryId" class="text-sm">Category <span class="text-danger">*</span></label>
+                <select id="categoryId" class="form-control form-control-sm">
+                    <option value="">- Select achievement scope</option>
+                    <option value="International">International</option>
+                    <option value="National">National</option>
+                    <option value="Regional">Regional</option>
+                </select>
+                <span class="text-xs text-danger" id="categoryIdError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementEventStart" class="text-sm">Event Start <span class="text-danger">*</span></label>
+                <input type="date" class="form-control form-control-sm" id="achievementEventStart">
+                <span class="text-xs text-danger" id="achievementEventStartError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column gap-2">
+                <label for="achievementEventEnd" class="text-sm">Event End <span class="text-danger">*</span></label>
+                <input type="date" class="form-control form-control-sm" id="achievementEventEnd">
+                <span class="text-xs text-danger" id="achievementEventEndError"></span>
+            </div>
+        </div>
+    </div>
     <div class="px-4 py-3 border-bottom border-secondary">
-        <h2 class="my-0 heading-6">User Details</h2>
+        <div class="d-flex align-items-center justify-content-between">
+            <h2 class="my-0 text-base font-semibold">Supervisor <span class="text-sm text-secondary font-medium">(Lecturer)</span></h2>
+            <button type="button" class="btn btn-sm btn-dark" id="btnAddNewSupervisor">
+                <div class="d-flex align-items-center gap-2">
+                    <i class="fa-solid fa-plus"></i>
+                    <span style="white-space: nowrap;">Add New</span>
+                </div>
+            </button>
+        </div>
     </div>
-    <div class="px-4 py-3 gy-3 row" id="userDetailsContainer"></div>
+    <div class="row gx-3 gy-2 px-4 py-3" id="supervisorContainer">
+        <div class="col-12 col-md-6">
+            <div class="d-flex align-items-center gap-2">
+                <select id="approvers" class="form-control form-control-sm w-100">
+                    <option value="">- Select lecturer</option>
+                    <option value="International">International</option>
+                    <option value="National">National</option>
+                    <option value="Regional">Regional</option>
+                </select>
+                <button class="btn btn-sm btn-danger">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+            <span class="text-xs text-danger mt-2" id="approversError"></span>
+        </div>
+    </div>
+    <div class="px-4 py-3 border-bottom border-secondary">
+        <h2 class="my-0 text-base font-semibold">Supporting Files</h2>
+    </div>
+    <div class="row gx-3 gy-2 px-4 py-3">
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column gap-2">
+                <label for="certificateFile" class="text-sm">Certificate <span class="text-danger">*</span></label>
+                <input type="file" class="form-control form-control-sm" id="certificateFile" accept=".jpg, .jpeg, .png, .pdf">
+                <span class="text-xs text-danger" id="certificateFileError"></span>
+            </div>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="d-flex flex-column gap-2">
+                <label for="assigmentFile" class="text-sm">Letter of Assigment <span class="text-danger">*</span></label>
+                <input type="file" class="form-control form-control-sm" id="assigmentFile" accept=".jpg, .jpeg, .png, .pdf">
+                <span class="text-xs text-danger" id="assigmentFileError"></span>
+            </div>
+        </div>
+    </div>
+    <div class="px-3 py-3 mt-4 border-top border-secondary">
+        <div class="d-flex align-items-center gap-2 justify-content-end">
+            <button type="button" class="btn btn-sm btn-danger">Cancel</button>
+            <button type="button" class="btn btn-sm btn-primary" id="btnSubmitAchievement">Submit</button>
+        </div>
+    </div>
 </section>
 
 <?php
